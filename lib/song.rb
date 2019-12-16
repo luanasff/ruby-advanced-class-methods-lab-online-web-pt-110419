239d0eae383a5fb214aa28aec352bae685264911
Luana Faria Lane <luanasfff@gmail.com>
2019-12-16 23:16:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,9 +35,20 @@ class Song
       song.name = name 
       song.save
       song 
+      end 
+     
+    def self.find_by_name(name)
       
-  
-  end 
+      
+    end 
+     
+     
+    def self.find_or_create_by_name(name)
+      
+      
+    end 
+     
+     
 end 
 
 
