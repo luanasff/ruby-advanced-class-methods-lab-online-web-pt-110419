01a951b6bb07cdee085cf8b46175a057cba23ec6
Luana Faria Lane <luanasfff@gmail.com>
2019-12-16 22:36:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,36 @@
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+   def initialize(name)
+    @name = name
+    end
+
+
+
+
   def self.all
     @@all
   end
-
+  
+  
   def save
     self.class.all << self
   end
+ 
+ def self.create 
+    song = self.new 
+    song.save 
+    song
+  end  
 
-end
+
+  def self.new_by_name(name)
+    @name = name 
+    self.new_by_name(name)
+  end
+  
+end 
+
+
